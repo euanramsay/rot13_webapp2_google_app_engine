a5d765959e9fa07aa362e1492c2348063b393977
Euan Ramsay <euancramsay@aol.com>
2017-04-26 12:59:46 +0100

    removed gitignore files

--- rot_function.py
+++
@@ -1,7 +0,0 @@
-import string
-
-def rot13(text):
-    rot13 = string.maketrans("ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
-                                "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
-       
-    return string.translate(text, rot13)
